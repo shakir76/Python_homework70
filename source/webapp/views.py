f0f3b81f8174a10ef1f01cee8a51498cf5575d93
kirillgrekhovodov <kirillgrekhovodov@gmail.com>
2022-06-21 19:40:51 +0600

    создал модель Article, создал и применил миграцию

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -14,15 +14,9 @@ def create_article(request):
     if request.method == "GET":
         return render(request, "create.html")
     else:
-        user = {
-            "name": "Test",
-            "age": 25,
-            "email": "test@mail.ru"
-        }
         context = {
             "title": request.POST.get("title"),
             "author": request.POST.get("author"),
             "content": request.POST.get("content"),
-            "user": user
         }
         return render(request, "article_view.html", context)
