0877369c97f121d868c12076b8d49286466d5057
kirillgrekhovodov <kirillgrekhovodov@gmail.com>
2022-06-24 21:38:28 +0600

    В urls добавили имена каждому урлу, так же для детального просмотра передаем pk

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from webapp.views import index_view, create_article, article_view
 
 urlpatterns = [
-    path('', index_view),
-    path('articles/add/', create_article),
-    path('article/', article_view)
+    path('', index_view, name="index"),
+    path('articles/add/', create_article, name="create_article"),
+    path('article/<int:pk>/', article_view, name="article_view")
 ]
