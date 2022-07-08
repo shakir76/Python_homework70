abd273aca7e55c3506790efdf663bbbcacb94c22
kirillgrekhovodov <kirillgrekhovodov@gmail.com>
2022-07-08 21:00:26 +0600

    перенесли данные из таблицы article_tag в подобную ей, но без использования свобственной промежуточной таблицы, а используя возможности джанго, добавили в форму тэги

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from django.forms import widgets
 
+from webapp.models import Tag
+
 
 class ArticleForm(forms.Form):
     title = forms.CharField(max_length=50, required=True, label='Title')
     author = forms.CharField(max_length=50, required=True, label='Author')
     content = forms.CharField(max_length=3000, required=True, label='Content',
                               widget=widgets.Textarea(attrs={"cols": 40, "rows": 3}))
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(),
+                                          required=False, label='Teg')
